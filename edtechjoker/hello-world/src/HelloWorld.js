e8b816182d5df11b8b0142dce5de993123441542
Khory <khorymul@gmail.com>
2021-09-05 20:10:39 -0400

    v1.0.0

---
+++ edtechjoker/hello-world/src/HelloWorld.js
@@ -0,0 +1,51 @@
+import { html, css, LitElement } from 'lit';
+
+export class HelloWorld extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        padding: 25px;
+        color: var(--hello-world-text-color, #000);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      counter: { type: Number },
+    };
+  }
+
+  constructor() {
+    super();
+    this.title = 'Hey there';
+    this.counter = 5;
+  }
+
+  __increment() {
+    this.counter += 1;
+  }
+
+  __decrement(event) {
+    let element = event.target;
+    if (this.counter > 0)  {
+      this.counter -= 1;
+      if (this.counter == 0) {
+        element.disabled = true;
+      }
+    }
+  
+  }
+
+  
+
+  render() {
+    return html`
+      <h2>${this.title} Nr. ${this.counter}!</h2>
+      <button @click=${this.__increment}>increment</button>
+      <button @click=${this.__decrement}>decrement</button>
+    `;
+  }
+}
